db23ad0885d4592af9ac70c6f34bc1b7c6d69250
goncacp <goncacp@gmail.com>
2015-11-30 16:20:59 +0100

    plot matlab changed

--- plot_map_project.m
+++ plot_map_project.m
@@ -10,3 +10,9 @@ for i=1:length(map(:,1))
         end
     end
 end
+
+links=importdata('links_goncas.txt');
+
+for i=1:length(links(:,1))
+    plot([links(i,1)*100,links(i,3)*100],[links(i,2)*100,links(i,4)*100],'Color','g');
+end
