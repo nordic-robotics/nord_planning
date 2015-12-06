adba62473d8eb4d66d89e3d35355e354c65997d2
TobiasLundin <tobbesayssend2me@hotmail.com>
2015-12-06 18:47:59 +0100

    forgot about this file..

--- src/nord_planning.cpp
+++ src/nord_planning.cpp
@@ -5,15 +5,25 @@
 #include <string>
 #include "graph/graph.hpp"
 
+//Run with 0 as input for Tobias, 1 for Lucas
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "nord_planning");
     ros::NodeHandle n;
-	
-	graph::Maps run;
+	bool dijk = true;
+	graph::Maps run(dijk);
 	
 	run.move_points();
 	run.create_graph();
 	run.print_info();
+
+	dijk = false;
+	std::cout << "run 2" << std::endl;
+	graph::Maps run2(dijk);
+	run2.move_points();
+	run2.create_graph();
+	run2.print_info();
+		
     return 0;
 }
